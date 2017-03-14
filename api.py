98d05e8ba7d62b78a3efc1b265a16fdb4744c53a
arnaldocorreiafilho <arnaldocorreiadeandradefilho@gmail.com>
2017-03-14 07:47:23 +0000

    simulador tesouro heroku python 2.7.12

--- api.py
+++ api.py
@@ -17,4 +17,7 @@ def hello_world():
 def get_colors():
     return tesouro.get_data()
 
-app.run()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
